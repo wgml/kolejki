c0ecf16ed7d983a7e81d60dd8d8b97f50b1c5690
vka <voodkapl@gmail.com>
2013-11-14 15:41:31 +0100

    start

---
+++ kolejki.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : kolejki.cpp
+// Author      : vka
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "" << endl; // prints 
+	return 0;
+}
